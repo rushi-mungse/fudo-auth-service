20fa9e32e3c1ca1e49f50380deb29e8400a7e21d
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-16 23:48:23 +0530

    feat: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "no-console": "error",
+  },
+  root: true,
+};
