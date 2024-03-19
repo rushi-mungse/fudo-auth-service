7fe1944a42d1fd856f6127ce7d84a0bdfb947332
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-19 16:33:37 +0530

    feat: add errorr middleware

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,8 +1,13 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import { HttpError } from "http-errors"
 import logger from "../config/logger"
 
-export default function (err: HttpError, req: Request, res: Response) {
+export default function (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   logger.error(err.message)
   const statusCode = err.statusCode || err.status || 500
   res.status(statusCode).json({
