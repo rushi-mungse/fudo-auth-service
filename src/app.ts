5d514ce33adaaf3b4a5d1b6ee0fdce7931aac8f4
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-19 16:31:34 +0530

    feat: add cookieparser, bodyparser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+
+import authRouter from "./auth/routes"
 import errorMiddleware from "./middleware/error"
 
 const app = express()
 
-app.get("/", (req, res) => {
-  return res.send({ message: "all ok!" })
-})
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static("public"))
 
+app.use("/api/auth", authRouter)
 app.use(errorMiddleware)
+
 export default app
