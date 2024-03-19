dbccdcc000d42555001c4d983f6cc95d27d6a232
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-19 16:33:52 +0530

    feat: add invalid token middleware

---
+++ src/middleware/invalid-token.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Response, Request } from "express"
+import createHttpError from "http-errors"
+
+import { ICookieData } from "../types"
+import TokenService from "../service/token"
+import TokenModel from "../model/token"
+
+const tokenService = new TokenService(TokenModel)
+
+const invalidToken = function (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { refreshToken } = req.cookies as ICookieData
+  if (!refreshToken) return next()
+
+  try {
+    const token = tokenService.verifyRefreshToken(refreshToken)
+    if (token) return next(createHttpError(400, "User already login!"))
+    return next()
+  } catch (error) {
+    return next(error)
+  }
+}
+
+export default invalidToken
