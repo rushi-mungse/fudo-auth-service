49e4facaebc5b2d1da992e033952ef18ba1ed454
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-19 16:35:29 +0530

    fix: delete app test

--- tests/app.test.ts
+++
@@ -1,9 +0,0 @@
-import request from "supertest"
-import app from "../src/app"
-
-describe("GET:", () => {
-  it("should return 200 status code", async () => {
-    const response = await request(app).get("/")
-    expect(response.statusCode).toBe(200)
-  })
-})
