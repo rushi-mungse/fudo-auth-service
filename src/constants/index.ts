fed43ed8d568c1077a27717a742881c45769c8b6
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-20 14:06:49 +0530

    feat: add constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,6 @@ export const UserRoles = {
   ADMIN: "admin",
   CUSTOMER: "customer",
 } as const
+
+export const PriceType = ["base", "aditional"] as const
+export const WidgetType = ["switch", "radio"] as const
