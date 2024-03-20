2c7677eaa41597feb433f98793e9648e5b4500d7
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-20 14:09:01 +0530

    feat: add product routes

--- src/features/products/type.ts
+++ src/features/products/type.ts
@@ -1,11 +1,41 @@
-import { ICategory } from "../category/type"
+import { Document } from "mongoose"
+import { ICategoryModal } from "../category/type"
 
+export interface IAttribute {
+  name: string
+  value: string
+}
+
+export interface IPriceConfiguration {
+  [key: string]: {
+    priceType: "base" | "aditional"
+    availableOptions: {
+      [key: string]: number
+    }
+  }
+}
+
+export interface IProductBody {
+  name: string
+  description: string
+  image: string
+  isPublish: string
+  discount: string
+  categoryId: string
+  preparationTime: string
+  attributes: string
+  priceConfiguration: string
+}
 export interface IProduct {
   name: string
   description: string
-  availability: string
-  imageUrl: string
-  preparationTime: number
+  image: string
+  isPublish: boolean
   discount: number
-  categories: ICategory[]
+  categoryId: ICategoryModal["_id"]
+  preparationTime: number
+  attributes: IAttribute[]
+  priceConfiguration: IPriceConfiguration
 }
+
+export type IProductModel = IProduct & Document
