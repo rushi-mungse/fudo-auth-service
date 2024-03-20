187ead4a1b9e9042f9d41402d2aabaa670463e23
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-20 14:06:34 +0530

    feat: add product route

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ import bodyParser from "body-parser"
 import authRouter from "./features/auth/routes"
 import errorMiddleware from "./middleware/error"
 import categoryRouter from "./features/category/routes"
+import productRouter from "./features/products/router"
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cookieParser())
 app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(express.static("public"))
 
 app.use("/api/auth", authRouter)
 app.use("/api/category", categoryRouter)
+app.use("/api/product", productRouter)
 app.use(errorMiddleware)
 
 export default app
