1793c6536ec9fa885e78a9f298236d00ab3c401d
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-04-04 22:25:26 +0530

    fix: invalid token

--- src/middleware/invalid-token.ts
+++ src/middleware/invalid-token.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Response, Request } from "express"
 import createHttpError from "http-errors"
 
-import { ICookieData } from "../types"
+import { ICookieData, TJwtPayload } from "../types"
 import TokenService from "../service/token"
 import TokenModel from "../model/token"
 import logger from "../config/logger"
+import AuthService from "../features/auth/service"
+import UserModel from "../features/auth/model"
 
 const tokenService = new TokenService(TokenModel)
+const userService = new AuthService(UserModel)
 
 const invalidToken = function (
   req: Request,
@@ -16,7 +19,10 @@ const invalidToken = function (
   const { refreshToken } = req.cookies as ICookieData
   if (!refreshToken) return next()
   try {
-    const token = tokenService.verifyRefreshToken(refreshToken)
+    const token = tokenService.verifyRefreshToken(refreshToken) as TJwtPayload
+    const userId = token.userId
+    const user = userService.getById(userId)
+    if (!user) return next()
     logger.info(token)
     if (token) return next(createHttpError(400, "User already login!"))
     return next()
